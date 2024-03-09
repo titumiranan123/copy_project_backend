8b6713c49528b91d4bed2ca532729b83698e450a
MD Titumir Anan <titumiranan.gtc@gmail.com>
2024-03-09 17:52:54 +0600

    start route

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-import { Request } from 'express'
+import { Request, Response } from 'express'
 import { app } from './src/app'
 
 app.get('/', (req, res) => {
-  res.send('server is runnign')
+  res.send('server is running')
+})
+app.get('/start', (req, res: Response) => {
+  res.send('server is running with no issues ')
 })
 
 app.listen(process.env.port, () => {
